e94f8a6afae245fe280d1848e01e291e6042f2b0
Tudor Teisanu <teisanutudort@gmail.com>
2022-01-13 19:28:01 +0200

    remove unused const webpack

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -5,7 +5,6 @@ const isProduction = process.env.NODE_ENV === 'production';
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const autoprefixer = require("autoprefixer");
-const webpack = require('webpack');
 
 module.exports = {
     mode: process.env.NODE_ENV,
